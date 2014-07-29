735b255eb045a76f718c9bd42496309bb5feec6c
Mia Garbagnati <fgarbagnati@Mia-Garbagnatis-MacBook-Pro.local>
2014-07-29 14:22:15 -0700

    added route for new restaurant button

--- site/js/routers/router.js
+++ site/js/routers/router.js
@@ -1,12 +1,16 @@
 var Router = Backbone.Router.extend({
   routes: {
-    '':'home'
+    '':'home',
+    'new': 'editRestaurant'
   }
 });
 
 var router = new Router();
 router.on('route:home', function() {
   console.log('in index route');
+});
+router.on('route:editRestaurant', function() {
+  console.log('show user form');
 });
 
 Backbone.history.start();
