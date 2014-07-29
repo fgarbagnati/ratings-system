d3d7349657f2e921181c750c0a563d38e3d20da0
Mia Garbagnati <fgarbagnati@Mia-Garbagnatis-MacBook-Pro.local>
2014-07-29 14:17:14 -0700

    eliminated need for scripts.js file by putting routes in their own file

--- site/js/scripts.js
+++ site/js/scripts.js
@@ -1,13 +1,4 @@
 
-var Router = Backbone.Router.extend({
-  routes: {
-    '':'home'
-  }
-});
 
-var router = new Router();
-router.on('route:home', function() {
-  console.log('in index route');
-});
 
-Backbone.history.start();
+// Backbone.history.start();
