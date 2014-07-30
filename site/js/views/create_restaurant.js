ce11bb6268b588078c419bfb414d0d7e13d29713
fgarbagnati <f.g.garbagnati@gmail.com>
2014-07-29 18:21:11 -0700

    created render function for adding new restaurant

---
+++ site/js/views/create_restaurant.js
@@ -0,0 +1,9 @@
+var AddRestaurant = Backbone.View.extend({
+  el: '.page',
+  render: function() {
+    var template = _.template($('#add-restaurant-template').html(), {});
+    this.$el.html(template);
+      }
+    })
+  }
+});
